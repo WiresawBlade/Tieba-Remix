82d0084926b6112b5931361ab5b7d6eee8811f76
WiresawBlade <wiresaw_blade@126.com>
2023-02-15 22:51:17 +0800

    项目目录更新

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,17 +21,18 @@ module.exports = {
         "linebreak-style": [2, "windows"],
         "quotes": [2, "double"],
         "semi": [2, "always"],
-        "@typescript-eslint/triple-slash-reference": 0,  /* ref */
+        "@typescript-eslint/triple-slash-reference": 0,               /* ref */
         "no-var": 1,
         "strict": 0,
         "spaced-comment": 0,
         "no-undef": 0,
-        "radix": 0,  /* praseInt radix */
-        "@typescript-eslint/no-unused-expressions": 0,  /* 禁止无效表达式 */
+        "radix": 0,                                                   /* praseInt radix */
+        "@typescript-eslint/no-unused-expressions": 0,                /* 禁止无效表达式 */
         "no-param-reassign": 0,
         "@typescript-eslint/no-non-null-asserted-optional-chain": 0,  /* 变量不能为 null */
-        "@typescript-eslint/ban-ts-comment": 0,  /* ts ignore 等 */
-        "comma-dangle": [2, "never"],  /* 最后一个逗号 */
-        "@typescript-eslint/no-unused-vars": 0  /* 从未使用过的变量，不交给 ESLint 处理 */
+        "@typescript-eslint/ban-ts-comment": 0,                       /* ts ignore 等 */
+        "comma-dangle": [2, "never"],                                 /* 最后一个逗号 */
+        "@typescript-eslint/no-unused-vars": 0,                       /* 从未使用过的变量，不交给 ESLint 处理 */
+        "eqeqeq": 2
     }
 };
