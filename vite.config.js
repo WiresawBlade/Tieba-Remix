91dec08713b9e50579e9363b0c0605c47662c033
WiresawBlade <wiresaw_blade@126.com>
2023-03-11 23:30:14 +0800

    配置更新

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,16 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import banner from "vite-plugin-banner";
+import postcssPresetEnv from "postcss-preset-env";
+import deepmerge from "deepmerge";
 
 import meta from "./meta.json";
 
-export default defineConfig({
-    build: {
-        lib: {
-            entry: "./src/main.ts",
-            name: "userscript",
-            formats: ["iife"],
-            fileName: () => `tieba-remix.user.js`
-        }
-    },
-    plugins: [
-        banner(() => {
-            return toMetaString(meta);
-        })
-    ],
-    resolve: {
-        alias: [
-            {
-                find: "@",
-                replacement: resolve("./src")
-            }
-        ]
-    },
-    server: {
-        hmr: {
-            protocol: "ws",
-            host: "localhost"
-        }
-    }
-});
-
+/**
+ * 通过 meta对象生成 meta字符串
+ * @param { {} } obj meta 对象
+ * @returns { string }
+ */
 function toMetaString(obj) {
     let metaString = "// ==UserScript==\n";
     const maxLength = Object.keys(obj).reduce((prev, curr) => Math.max(prev, curr.length), 0);
@@ -46,7 +23,7 @@ function toMetaString(obj) {
         return str;
     }
 
-    for (let key in obj) {
+    for (const key in obj) {
         if (Array.isArray(obj[key])) {
             for (let i = 0; i < obj[key].length; i++) {
                 metaString += "// @" + key + spaces(maxLength - key.length + 2) + obj[key][i] + "\n";
@@ -55,6 +32,86 @@ function toMetaString(obj) {
             metaString += "// @" + key + spaces(maxLength - key.length + 2) + obj[key] + "\n";
         }
     }
-    metaString += "// ==/UserScript==\n" + "// @WiresawBlade\n";
+    metaString += "// ==/UserScript==\n" + `// @WiresawBlade\n`;
     return metaString;
 }
+
+const commonConfig =  defineConfig({
+    build: {
+        lib: {
+            entry: "./src/main.ts",
+            name: "TiebaRemix",
+            formats: ["iife"],
+            fileName: () => `tieba-remix.user.js`
+        },
+        reportCompressedSize: false
+    },
+    plugins: [
+        banner(() => {
+            return toMetaString(meta);
+        })
+    ],
+    css: {
+        postcss: {
+            plugins: [
+                postcssPresetEnv()
+            ]
+        }
+    },
+    resolve: {
+        alias: [
+            {
+                find: "@",
+                replacement: resolve(__dirname, "./src")
+            },
+            {
+                find: "@lib",
+                replacement: resolve(__dirname, "./src/lib")
+            },
+            {
+                find: "@modules",
+                replacement: resolve(__dirname, "./src/modules")
+            }
+        ]
+    },
+    server: {
+        hmr: {
+            protocol: "ws",
+            host: "localhost"
+        },
+        host: "0.0.0.0"
+    }
+});
+
+const devConfig = defineConfig({
+    build: {
+        minify: "terser",
+        terserOptions: {
+            sourceMap: true
+        }
+    }
+});
+
+const prodConfig = defineConfig({
+    build: {
+        minify: "terser",
+        terserOptions: {
+            sourceMap: false,
+            toplevel: true,
+            compress: {
+                pure_funcs: [
+                    "console.log"
+                ]
+            }
+        }
+    }
+});
+
+const viteConfig = {
+    "serve": () => { return deepmerge(commonConfig, devConfig); },
+    "build": () => { return deepmerge(commonConfig, prodConfig); }
+};
+
+export default defineConfig(({ command }) => {
+    return viteConfig[command]();
+});
