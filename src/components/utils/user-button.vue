ce8aef663cc60341d0fc8027151dfa16377ab6c9
WiresawBlade <wiresaw_blade@126.com>
2023-04-29 18:49:39 +0800

    基本组件更新

---
+++ src/components/utils/user-button.vue
@@ -0,0 +1,39 @@
+<template>
+    <button class="user-button" type="button" :title="props.title">
+        {{ props.title }}
+        <slot></slot>
+    </button>
+</template>
+
+<script setup lang="ts">
+interface Props {
+    title?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    title: ""
+});
+</script>
+
+<style scoped lang="scss">
+@use "@/stylesheets/main/palette" as _;
+
+.user-button {
+    box-sizing: border-box;
+    padding: 2px 8px;
+    border: none;
+    border: 1px solid _.$borderColor;
+    border-radius: 6px;
+    background: none;
+    background-color: _.$defaultBack;
+    cursor: pointer;
+}
+
+.user-button:hover {
+    background-color: _.$elemColor;
+}
+
+.user-button:focus {
+    border: 2px solid _.$tiebaThemeColor;
+}
+</style>
---
+++ src/components/utils/user-textbox.vue
@@ -0,0 +1,37 @@
+<template>
+    <input class="user-textbox" type="text" :value="modelValue"
+        @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)" autocomplete="off">
+</template>
+
+<script setup lang="ts">
+interface Props {
+    modelValue?: string
+}
+
+withDefaults(defineProps<Props>(), {
+    modelValue: ""
+});
+
+defineEmits(["update:modelValue"]);
+</script>
+
+<style scoped lang="scss">
+@use "@/stylesheets/main/palette" as _;
+
+.user-textbox {
+    box-sizing: border-box;
+    padding: 4px;
+    border: 2px solid _.$borderColor;
+    border-radius: 6px;
+    background-color: _.$defaultBack;
+    outline: none;
+}
+
+.user-textbox:hover {
+    border-color: _.$lightBack;
+}
+
+.user-textbox:focus {
+    border-color: _.$tiebaThemeColor;
+}
+</style>
