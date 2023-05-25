cd7ce84927420fc12653cca99f83b69a73667cee
WiresawBlade <wiresaw_blade@126.com>
2023-05-25 16:18:49 +0800

    菜单行为更改

--- src/greasy-init.ts
+++ src/greasy-init.ts
@@ -1,14 +1,7 @@
-export { greasyInit };
+import settingsVue from "./components/settings.vue";
+import { renderDialog } from "./lib/render";
 
-try {
-    // 开启或关闭某些功能
-    GM_deleteValue("ENABLE_BOLD_FONT");
-    GM_deleteValue("EXTREME_PURIF");
-    GM_deleteValue("DEFAULT_FONT_TYPE");
-    GM_deleteValue("userSwitches");
-} catch (error) {
-    console.warn(error);
-}
+export { greasyInit };
 
 interface MenuObject {
     id: string;
@@ -21,64 +14,72 @@ interface MenuObject {
 // 插件菜单
 export const greasyMenu: MenuObject[] = [
     {
+        id: "settings",
+        title: "设置",
+        type: "button",
+        event() {
+            renderDialog(settingsVue);
+        }
+    },
+    {
         id: "checkUpdate",
-        title: "获取更新...",
+        title: "开放源代码...",
         type: "button",
         event: () => {
             GM_openInTab("https://github.com/WiresawBlade/Tieba-Remix/releases/", {
                 active: true
             });
         }
-    },
-    {
-        id: "setShieldList",
-        title: "设置屏蔽列表",
-        type: "button",
-        event: () => {
-            const shieldList = GM_getValue("shieldList", <ShieldObject[]>[]);
-            const listStr = (() => {
-                let str = "";
-                if (shieldList.length > 0) {
-                    for (const sh of shieldList) {
-                        str += sh.rule + "\n";
-                    }
-                    return str;
-                } else {
-                    return "\n";
-                }
-            })();
-            const userInput = prompt(`
-            设置屏蔽列表（临时方案）
-            当前共有 ${shieldList.length} 条屏蔽规则被装载
-            ${listStr}
-            输入需要匹配的屏蔽词，它们目前会在首页动态和贴子楼层生效
-            输入“REMOVE_ALL”清除当前所有屏蔽词
-            `.split("    ").join(""));
-
-            if (userInput !== null && userInput !== "") {
-                if (userInput === "REMOVE_ALL") {
-                    GM_deleteValue("shieldList");
-                    location.reload();
-                    return;
-                }
-
-                for (const sh of shieldList) {
-                    if (sh.rule === userInput) {
-                        location.reload();
-                        return;
-                    }
-                }
-                shieldList.push({
-                    rule: userInput,
-                    type: "string",
-                    scope: "posts",
-                    switch: true
-                });
-                GM_setValue("shieldList", shieldList);
-                location.reload();
-            }
-        }
     }
+    // {
+    //     id: "setShieldList",
+    //     title: "设置屏蔽列表",
+    //     type: "button",
+    //     event: () => {
+    //         const shieldList = GM_getValue("shieldList", <ShieldObject[]>[]);
+    //         const listStr = (() => {
+    //             let str = "";
+    //             if (shieldList.length > 0) {
+    //                 for (const sh of shieldList) {
+    //                     str += sh.rule + "\n";
+    //                 }
+    //                 return str;
+    //             } else {
+    //                 return "\n";
+    //             }
+    //         })();
+    //         const userInput = prompt(`
+    //         设置屏蔽列表（临时方案）
+    //         当前共有 ${shieldList.length} 条屏蔽规则被装载
+    //         ${listStr}
+    //         输入需要匹配的屏蔽词，它们目前会在首页动态和贴子楼层生效
+    //         输入“REMOVE_ALL”清除当前所有屏蔽词
+    //         `.split("    ").join(""));
+
+    //         if (userInput !== null && userInput !== "") {
+    //             if (userInput === "REMOVE_ALL") {
+    //                 GM_deleteValue("shieldList");
+    //                 location.reload();
+    //                 return;
+    //             }
+
+    //             for (const sh of shieldList) {
+    //                 if (sh.rule === userInput) {
+    //                     location.reload();
+    //                     return;
+    //                 }
+    //             }
+    //             shieldList.push({
+    //                 rule: userInput,
+    //                 type: "string",
+    //                 scope: "posts",
+    //                 switch: true
+    //             });
+    //             GM_setValue("shieldList", shieldList);
+    //             location.reload();
+    //         }
+    //     }
+    // }
 ];
 
 // 注册菜单
