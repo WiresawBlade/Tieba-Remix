1d1f3aa4797cee9195ac28df56f584530d816074
WiresawBlade <wiresaw_blade@126.com>
2023-07-11 16:27:11 +0800

    增加设置组件

--- src/modules/remixed-toolkit/entry.ts
+++ src/modules/remixed-toolkit/entry.ts
@@ -1,7 +1,10 @@
 import { forEach } from "lodash-es";
 import { DOMS } from "@/lib/domlib";
+import { remixedObservers } from "@/lib/observers";
+import { UserModuleExtended } from "@/global.module";
+import { ref } from "vue";
 
-export const Main: UserModule = {
+export const Main: UserModuleExtended = {
     id: "remixed-toolkit",
     name: "实用工具库",
     author: "锯刃Blade",
@@ -10,18 +13,44 @@ export const Main: UserModule = {
     description: "这是一个轻量级的工具库，包含了诸如自动展开长图等实用功能。",
     scope: true,
     runAt: "immediately",
+    settings: {
+        "auto-expand": {
+            title: "自动展开长图",
+            description:
+                `该功能会自动将帖子中所有的长图片自动展开，无需手动操作`,
+            widgets: {
+                type: "toggle",
+                init: () => toolkitToogles.value["auto-expand"],
+                event() {
+                    toolkitToogles.value["auto-expand"] = !toolkitToogles.value["auto-expand"];
+                    GM_setValue<ToolkitToogles>("toolkitToogles", toolkitToogles.value);
+                }
+            }
+        }
+    },
     entry: function () {
-        autoExpand();
+        for (const key in toolkitFeatures) {
+            // @ts-ignore
+            if (toolkitToogles.value[key]) toolkitFeatures[key]();
+        }
     }
 };
 
-/**
- * 自动展开长图
- */
-function autoExpand() {
-    window.addEventListener("load", () => {
-        forEach(DOMS(".replace_tip"), (el) => {
-            (el as HTMLDivElement).click();
+const toolkitFeatures = {
+    /**
+     * 自动展开长图
+     */
+    "auto-expand"() {
+        remixedObservers.postsObserver.addEvent(() => {
+            forEach(DOMS(".replace_tip"), (el) => {
+                (el as HTMLDivElement).click();
+            });
         });
-    });
-}
+    }
+};
+
+type ToolkitToogles = KeyMapped<typeof toolkitFeatures, boolean>
+
+const toolkitToogles = ref(GM_getValue<ToolkitToogles>("toolkitToogles", {
+    "auto-expand": true
+}));
