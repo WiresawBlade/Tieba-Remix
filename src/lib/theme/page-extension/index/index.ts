6eb7d24dffffd2f6f38ce810ec6c3cb20442fc60
WiresawBlade <wiresaw_blade@126.com>
2024-02-06 18:07:14 +0800

    feat: 新版主页统一加载方法

---
+++ src/lib/theme/page-extension/index/index.ts
@@ -0,0 +1,25 @@
+import { GM_addStyle } from "$";
+import indexVue from "@/components/pages/index.vue";
+import { currentPageType } from "@/lib/api/remixed";
+import { DOMS } from "@/lib/elemental";
+import { parseMultiCSS } from "@/lib/elemental/styles";
+import { removeDefault, renderPage } from "@/lib/render";
+import { pageExtension } from "@/lib/user-values";
+import { waitUtil } from "@/lib/utils";
+
+export default async function () {
+    if (currentPageType() !== "index") return;
+    if (!pageExtension.get().index) return;
+
+    const bodyMask = GM_addStyle(parseMultiCSS({
+        "body": {
+            display: "none",
+        },
+    }));
+    removeDefault();
+
+    await waitUtil(() => DOMS(true, ".wrap1") !== null, undefined, 4);
+    renderPage(indexVue);
+    DOMS(true, ".wrap1").remove();
+    bodyMask.remove();
+}
