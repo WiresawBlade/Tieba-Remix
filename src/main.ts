74e5eb702ac9b3e057f06f851c4419db39232b08
WiresawBlade <wiresaw_blade@126.com>
2024-02-06 18:07:33 +0800

    perf: 优化首屏加载

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,12 @@
 import "element-plus/dist/index.css";
-import indexVue from "./components/pages/index.vue";
 import { checkUpdateAndNotify, currentPageType, setTheme } from "./lib/api/remixed";
-import { DOMS, afterHead } from "./lib/elemental";
-import { injectCSSList } from "./lib/elemental/styles";
 import { remixedObservers } from "./lib/observers";
-import { removeDefault, renderPage } from "./lib/render";
 import { loadBaseCSS, loadDynamicCSS, loadExtensionCSS, loadTiebaCSS } from "./lib/theme";
-import { parseUserModules } from "./lib/unsafe";
-import { REMIXED, pageExtension, themeType } from "./lib/user-values";
-import { AllModules, waitUtil } from "./lib/utils";
+import index from "./lib/theme/page-extension/index";
 import thread from "./lib/theme/page-extension/thread";
+import { parseUserModules } from "./lib/unsafe";
+import { REMIXED, themeType } from "./lib/user-values";
+import { AllModules } from "./lib/utils";
 
 setTheme(themeType.get());
 loadBaseCSS();
@@ -18,36 +15,8 @@ Promise.all([
     loadDynamicCSS(),
     loadTiebaCSS(),
     loadExtensionCSS(),
+    index(),
     thread(),
-    (async function loadNewIndex() {
-        if (!pageExtension.get().index) return;
-        if (currentPageType() !== "index") return;
-
-        afterHead(function () {
-            const bodyMask = injectCSSList(`
-            body {
-                display: none;
-            }
-        `);
-
-            injectCSSList(`
-            #com_userbar {
-                display: none;
-            }
-
-            .tbui_aside_float_bar {
-                display: none;
-            }
-        `);
-
-            waitUtil(() => document.body !== null).then(function () {
-                renderPage(indexVue);
-                DOMS(".wrap1")[0].remove();
-                removeDefault();
-                bodyMask.remove();
-            });
-        });
-    })(),
     (async function loadUserModules() {
         let index = 0;
         parseUserModules(
