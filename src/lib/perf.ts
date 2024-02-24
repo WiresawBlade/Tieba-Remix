3e604792d1f1531069b6a20a9bd170a030e49c7b
WiresawBlade <wiresaw_blade@126.com>
2024-02-24 14:12:07 +0800

    fix: 修复懒加载配置可能被原版贴吧配置覆盖的问题

--- src/lib/perf.ts
+++ src/lib/perf.ts
@@ -9,6 +9,9 @@ export function loadPerf() {
     setThreadLazyload();
 }
 
+/**
+ * 根据性能配置对 `<html>` 标签添加对应的属性开关，供 CSS 等进行使用
+ */
 export function setPerfAttr() {
     const perfAttr: Record<PerfType, string> = {
         default: "perf-default",
@@ -26,6 +29,8 @@ export function setPerfAttr() {
 
 /**
  * 帖子页面懒加载性能配置
+ * 
+ * 针对不同性能配置，对楼中楼懒加载范围进行调整。高性能模式下会直接加载整页的评论，以减少视觉抖动；而节能配置被设定为贴吧默认值 (500).
  */
 export async function setThreadLazyload() {
     if (currentPageType() !== "thread") return;
@@ -39,6 +44,9 @@ export async function setThreadLazyload() {
     remixedObservers.postsObserver.addEvent(setDiff);
 
     function setDiff() {
-        datalazyload.userConfig.diff = lazyloadDiff[perfProfile.get()];
+        // 立即生效可能会被贴吧再次覆盖为原始值，所以延迟一段时间
+        setTimeout(() => {
+            datalazyload.userConfig.diff = lazyloadDiff[perfProfile.get()];
+        }, 500);
     }
 }
